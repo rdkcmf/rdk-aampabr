e8ee4cce311649dad386e49a1ac2feeaf940781b
Nandakishor Udiyannur <Nandakishor_UdiyannurMana@comcast.com>
2020-11-11 06:43:32 -0500

    DELIA-45437 :[Field] [Data-Analysis] Bit rate shifting without any network changes during new period update & profile change
    Reason for change: Introducing ID based profile map to distiguish
    profiles under different periods IDs.
    Risks: Low
    Test Procedure: Test with STBs.
    Signed-off-by: Nandakishor Udiyannur<Nandakishor_udiyannurmana@comcast.com>
    
    Change-Id: Iee0ac78fb9d0cbb49bf318e4cf9f799c860c9ba0
    (cherry picked from commit 5d6ee9d711ebfc5c67f7b09b7a35fdfff0affff8)

--- ABRManager.cpp
+++ ABRManager.cpp
@@ -134,9 +134,18 @@ void ABRManager::initializeSortedBWProfileList() {
     for (int cnt = 0; cnt < profileCount; cnt++) {
       // store all the bandwidth data and its index to map which will sort by itself
       if (!mProfiles[cnt].isIframeTrack) {
-        mSortedBWProfileList[mProfiles[cnt].bandwidthBitsPerSecond] = cnt;
+        mSortedBWProfileList[mProfiles[cnt].periodId][mProfiles[cnt].bandwidthBitsPerSecond] = cnt;
       }
     }
+#if defined(DEBUG_ENABLED)
+    for(auto iter1 : mSortedBWProfileList) {
+      sLogger("%s: Period ID: %s\n", __FUNCTION__, iter1.first.c_str());
+      for(auto iter2 : iter1.second) {
+        sLogger("%s: bw:%ld idx:%d\n", __FUNCTION__, iter2.first, iter2.second);
+      }
+    }
+#endif
+
   }
 }
 
@@ -147,10 +156,11 @@ void ABRManager::initializeSortedBWProfileList() {
  * @param chooseMediumProfile Boolean flag, true means
  * to choose the medium profile, otherwise to choose the profile whose
  * bitrate >= the default bitrate.
+ * @param periodId empty string by default, Period-Id of profiles
  * 
  * @return The initial profile index 
  */
-int ABRManager::getInitialProfileIndex(bool chooseMediumProfile) {
+int ABRManager::getInitialProfileIndex(bool chooseMediumProfile, const std::string& periodId) {
   // This function will be called only once during session creation to get default profile
   // check if any profiles are added (incase), remove it before adding fresh
   // populate the container with sorted order of BW vs its index
@@ -162,13 +172,13 @@ int ABRManager::getInitialProfileIndex(bool chooseMediumProfile) {
   int desiredProfileIndex = INVALID_PROFILE;
   if (chooseMediumProfile && profileCount > 1) {
     // get the mid profile from the sorted list
-    SortedBWProfileListIter iter = mSortedBWProfileList.begin();
-    std::advance(iter, static_cast<int>(mSortedBWProfileList.size() / 2));
+    SortedBWProfileListIter iter = mSortedBWProfileList[periodId].begin();
+    std::advance(iter, static_cast<int>(mSortedBWProfileList[periodId].size() / 2));
     desiredProfileIndex = iter->second;
   } else {
     SortedBWProfileListIter iter;
-    desiredProfileIndex = mSortedBWProfileList.begin()->second;
-    for (iter = mSortedBWProfileList.begin(); iter != mSortedBWProfileList.end(); ++iter) {
+    desiredProfileIndex = mSortedBWProfileList[periodId].begin()->second;
+    for (iter = mSortedBWProfileList[periodId].begin(); iter != mSortedBWProfileList[periodId].end(); ++iter) {
       if (iter->first > mDefaultInitBitrate) {
         break;
       }
@@ -177,7 +187,7 @@ int ABRManager::getInitialProfileIndex(bool chooseMediumProfile) {
     }
   }
   if (INVALID_PROFILE == desiredProfileIndex) {
-    desiredProfileIndex = mSortedBWProfileList.begin()->second;
+    desiredProfileIndex = mSortedBWProfileList[periodId].begin()->second;
     sLogger("%s:%d Got invalid profile index, choose the first index = %d and profileCount = %d and defaultBitrate = %ld\n",
       __FUNCTION__, __LINE__, desiredProfileIndex, profileCount, mDefaultInitBitrate);
   } else {
@@ -327,9 +337,11 @@ int ABRManager::getBestMatchedProfileIndexByBandWidth(int bandwidth) {
  * @brief Ramp down the profile one step to get the profile index of a lower bitrate.
  *
  * @param currentProfileIndex The current profile index
+ * @param periodId empty string by default, Period-Id of profiles
+ *
  * @return the profile index of a lower bitrate (one step)
  */
-int ABRManager::getRampedDownProfileIndex(int currentProfileIndex) {
+int ABRManager::getRampedDownProfileIndex(int currentProfileIndex, const std::string& periodId) {
   // Clamp the param to avoid overflow
   int profileCount = getProfileCount();
   if (currentProfileIndex >= profileCount) {
@@ -345,13 +357,13 @@ int ABRManager::getRampedDownProfileIndex(int currentProfileIndex) {
     return desiredProfileIndex;
   }
   long currentBandwidth = mProfiles[currentProfileIndex].bandwidthBitsPerSecond;
-  SortedBWProfileListIter iter = mSortedBWProfileList.find(currentBandwidth);
-  if (iter == mSortedBWProfileList.end()) {
+  SortedBWProfileListIter iter = mSortedBWProfileList[periodId].find(currentBandwidth);
+  if (iter == mSortedBWProfileList[periodId].end()) {
     sLogger("%s:%d The current bitrate %ld is not in the profile list\n",
        __FUNCTION__, __LINE__, currentBandwidth);
     return desiredProfileIndex;
   }
-  if (iter == mSortedBWProfileList.begin()) {
+  if (iter == mSortedBWProfileList[periodId].begin()) {
     desiredProfileIndex = iter->second;
   } else {
     // get the prev profile . This is sorted list , so no worry of getting wrong profile 
@@ -370,9 +382,10 @@ int ABRManager::getRampedDownProfileIndex(int currentProfileIndex) {
  * @brief Ramp Up the profile one step to get the profile index of a upper bitrate.
  *
  * @param currentProfileIndex The current profile index
+ * @param periodId empty string by default, Period-Id of profiles
  * @return the profile index of a upper bitrate (one step)
  */
-int ABRManager::getRampedUpProfileIndex(int currentProfileIndex) {
+int ABRManager::getRampedUpProfileIndex(int currentProfileIndex, const std::string& periodId) {
   // Clamp the param to avoid overflow
   int profileCount = getProfileCount();
   int desiredProfileIndex = currentProfileIndex;
@@ -384,14 +397,14 @@ int ABRManager::getRampedUpProfileIndex(int currentProfileIndex) {
   }
   
   long currentBandwidth = mProfiles[currentProfileIndex].bandwidthBitsPerSecond;
-  SortedBWProfileListIter iter = mSortedBWProfileList.find(currentBandwidth);
-  if (iter == mSortedBWProfileList.end()) {
+  SortedBWProfileListIter iter = mSortedBWProfileList[periodId].find(currentBandwidth);
+  if (iter == mSortedBWProfileList[periodId].end()) {
     sLogger("%s:%d The current bitrate %ld is not in the profile list\n",
        __FUNCTION__, __LINE__, currentBandwidth);
     return desiredProfileIndex;
   }
 
-  if(std::next(iter) != mSortedBWProfileList.end())
+  if(std::next(iter) != mSortedBWProfileList[periodId].end())
   {
 	std::advance(iter, 1);
 	desiredProfileIndex = iter->second;
@@ -431,9 +444,10 @@ int ABRManager::getUserDataOfProfile(int currentProfileIndex)
  * @brief Check if the bitrate of currentProfileIndex reaches to the lowest.
  *
  * @param currentProfileIndex The current profile index
+ * @param periodId empty string by default, Period-Id of profiles
  * @return True means it reaches to the lowest, otherwise, it doesn't.
  */
-bool ABRManager::isProfileIndexBitrateLowest(int currentProfileIndex) {
+bool ABRManager::isProfileIndexBitrateLowest(int currentProfileIndex, const std::string& periodId) {
   // Clamp the param to avoid overflow
   int profileCount = getProfileCount();
   if (currentProfileIndex >= profileCount) {
@@ -451,8 +465,8 @@ bool ABRManager::isProfileIndexBitrateLowest(int currentProfileIndex) {
   }
 
   long currentBandwidth = mProfiles[currentProfileIndex].bandwidthBitsPerSecond;
-  SortedBWProfileListIter iter = mSortedBWProfileList.find(currentBandwidth);
-  return iter == mSortedBWProfileList.begin();
+  SortedBWProfileListIter iter = mSortedBWProfileList[periodId].find(currentBandwidth);
+  return iter == mSortedBWProfileList[periodId].begin();
 }
 
 /**
@@ -464,9 +478,10 @@ bool ABRManager::isProfileIndexBitrateLowest(int currentProfileIndex) {
  * @param currentBandwidth The current band width
  * @param networkBandwidth The current available bandwidth (network bandwidth)
  * @param nwConsistencyCnt Network consistency count, used for bitrate ramping up/down
+ * @param periodId empty string by default, Period-Id of profiles
  * @return int Profile index
  */
-int ABRManager::getProfileIndexByBitrateRampUpOrDown(int currentProfileIndex, long currentBandwidth, long networkBandwidth, int nwConsistencyCnt) {
+int ABRManager::getProfileIndexByBitrateRampUpOrDown(int currentProfileIndex, long currentBandwidth, long networkBandwidth, int nwConsistencyCnt, const std::string& periodId) {
   // Clamp the param to avoid overflow
   int profileCount = getProfileCount();
   if (currentProfileIndex >= profileCount) {
@@ -491,9 +506,9 @@ int ABRManager::getProfileIndexByBitrateRampUpOrDown(int currentProfileIndex, lo
   if(networkBandwidth > currentBandwidth) {
     // if networkBandwidth > is more than current bandwidth
     SortedBWProfileListIter iter;
-    SortedBWProfileListIter currIter = mSortedBWProfileList.find(currentBandwidth);
-    SortedBWProfileListIter storedIter = mSortedBWProfileList.end();
-    for (iter = currIter; iter != mSortedBWProfileList.end(); ++iter) {          
+    SortedBWProfileListIter currIter = mSortedBWProfileList[periodId].find(currentBandwidth);
+    SortedBWProfileListIter storedIter = mSortedBWProfileList[periodId].end();
+    for (iter = currIter; iter != mSortedBWProfileList[periodId].end(); ++iter) {
       // This is sort List 
       if (networkBandwidth >= iter->first) {
         desiredProfileIndex = iter->second;
@@ -504,7 +519,7 @@ int ABRManager::getProfileIndexByBitrateRampUpOrDown(int currentProfileIndex, lo
     }
 
     // No need to jump one profile for one network bw increase
-    if (storedIter != mSortedBWProfileList.end() && (currIter->first < storedIter->first) && std::distance(currIter, storedIter) == 1) {
+    if (storedIter != mSortedBWProfileList[periodId].end() && (currIter->first < storedIter->first) && std::distance(currIter, storedIter) == 1) {
       mAbrProfileChangeUpCount++;
       // if same profile holds good for next 3*2 fragments
       if (mAbrProfileChangeUpCount < nwConsistencyCnt) {
@@ -524,9 +539,9 @@ int ABRManager::getProfileIndexByBitrateRampUpOrDown(int currentProfileIndex, lo
   } else {
     // if networkBandwidth < than current bandwidth
     SortedBWProfileListRevIter reviter;
-    SortedBWProfileListIter currIter = mSortedBWProfileList.find(currentBandwidth);
-    SortedBWProfileListIter storedIter = mSortedBWProfileList.end();
-    for (reviter = mSortedBWProfileList.rbegin(); reviter != mSortedBWProfileList.rend(); ++reviter) {
+    SortedBWProfileListIter currIter = mSortedBWProfileList[periodId].find(currentBandwidth);
+    SortedBWProfileListIter storedIter = mSortedBWProfileList[periodId].end();
+    for (reviter = mSortedBWProfileList[periodId].rbegin(); reviter != mSortedBWProfileList[periodId].rend(); ++reviter) {
       // This is sorted List
       if (networkBandwidth >= reviter->first) {
         desiredProfileIndex = reviter->second;
@@ -538,13 +553,13 @@ int ABRManager::getProfileIndexByBitrateRampUpOrDown(int currentProfileIndex, lo
     }
 
     // we didn't find a profile which can be supported in this bandwidth
-    if (reviter == mSortedBWProfileList.rend()) {
-	desiredProfileIndex = mSortedBWProfileList.begin()->second;
+    if (reviter == mSortedBWProfileList[periodId].rend()) {
+	desiredProfileIndex = mSortedBWProfileList[periodId].begin()->second;
         sLogger("%s:%d Didn't find a profile which supports bandwidth[%ld], min bandwidth available [%ld]. Set profile to lowest!\n", __FUNCTION__, __LINE__, networkBandwidth, mSortedBWProfileList.begin()->first);
     }
 
     // No need to jump one profile for small  network change
-    if (storedIter != mSortedBWProfileList.end() && (currIter->first > storedIter->first) && std::distance(storedIter, currIter) == 1) {
+    if (storedIter != mSortedBWProfileList[periodId].end() && (currIter->first > storedIter->first) && std::distance(storedIter, currIter) == 1) {
       mAbrProfileChangeDownCount++;
       // if same profile holds good for next 3*2 fragments
       if(mAbrProfileChangeDownCount < nwConsistencyCnt) {
@@ -564,9 +579,9 @@ int ABRManager::getProfileIndexByBitrateRampUpOrDown(int currentProfileIndex, lo
   }
 
   if (currentProfileIndex != desiredProfileIndex) {
-    sLogger("%s:%d Current bandwidth[%ld] Network bandwidth[%ld] Current profile index[%d] Desired profile index[%d]\n",
+    sLogger("%s:%d Current bandwidth[%ld] Network bandwidth[%ld] Current profile index[%d] Desired profile index[%d] Period ID[%s]\n",
       __FUNCTION__, __LINE__, currentBandwidth, networkBandwidth,
-      currentProfileIndex, desiredProfileIndex);
+      currentProfileIndex, desiredProfileIndex, periodId.c_str());
   }
 
   return desiredProfileIndex;
@@ -598,9 +613,11 @@ long ABRManager::getBandwidthOfProfile(int profileIndex) {
 /**
  * @brief Get the index of max bandwidth
  * 
+ * @param periodId empty string by default, Period-Id of profiles
+ *
  * @return int index of the max bandwidth
  */
-int ABRManager::getMaxBandwidthProfile()
+int ABRManager::getMaxBandwidthProfile(const std::string& periodId)
 {
   int profileCount = getProfileCount();
   if (profileCount == 0) {
@@ -611,7 +628,7 @@ int ABRManager::getMaxBandwidthProfile()
   // Ensure the mSortedBWProfileList is populated
   initializeSortedBWProfileList();
 
-  return mSortedBWProfileList.size()?mSortedBWProfileList.rbegin()->second:0;
+  return mSortedBWProfileList[periodId].size()?mSortedBWProfileList[periodId].rbegin()->second:0;
 }
 
 // Getters/Setters
